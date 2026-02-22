54f558ef97535b0d5b4bc195121ec717534ee54f
Vihaan Kerekatte <vkerekatte@gmail.com>
2026-02-21 23:20:45 -0500

    Added better descriptions for each page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,7 +2,7 @@ export default function Signup() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 dark:bg-black">
       <h1 className="text-3xl font-bold text-black dark:text-white">Signup</h1>
-      <p className="text-zinc-600 dark:text-zinc-400">This is the signup page</p>
+      <p className="text-zinc-600 dark:text-zinc-400">This is the signup page. The page will include a form for new users to register.</p>
     </div>
   );
 }
