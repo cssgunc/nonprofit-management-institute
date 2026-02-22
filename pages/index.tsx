62858cba4eeda40a33b3bb24a1d6dc4db0656141
Vihaan Kerekatte <vkerekatte@gmail.com>
2026-02-21 23:27:35 -0500

    Added formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,12 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 dark:bg-black">
       <h1 className="text-3xl font-bold text-black dark:text-white">Home</h1>
-      <p className="text-zinc-600 dark:text-zinc-400">This is the home page, the main landing page for the Nonprofit Management Institute. Other pages are accessible currently by using /signup or /login and any unknown routes will be redirected to the 404 page.</p>
+      <p className="text-zinc-600 dark:text-zinc-400">
+        This is the home page, the main landing page for the Nonprofit
+        Management Institute. Other pages are accessible currently by using
+        /signup or /login and any unknown routes will be redirected to the 404
+        page.
+      </p>
     </div>
   );
 }
