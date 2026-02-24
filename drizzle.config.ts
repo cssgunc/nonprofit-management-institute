3ee235aeb33e1570acca48395bbe5eca6c7f5b76
dhruvnpUNC <dhruvnp@ad.unc.edu>
2026-02-24 18:42:36 -0500

    Initial Commit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "drizzle-kit";
+import { config } from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+
+config({ path: '.env' });
+
 export default defineConfig({
-  dialect: "postgresql",
-  schema: "./server/db/schema.ts",
-  out: "./supabase/migrations",
+  schema: './server/db/schema.ts',
+  out: './supabase/migrations',
+  dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
--- server/db/schema.ts
+++ server/db/schema.ts
@@ -6,7 +6,39 @@
  * npx drizzle-kit push
  * ```
  */
-import { pgTable, text } from "drizzle-orm/pg-core";
-import { relations } from "drizzle-orm";
+import { check, integer, pgTable, serial, text, timestamp, boolean} from "drizzle-orm/pg-core";
+import { relations, sql} from "drizzle-orm";
+import { config } from 'dotenv';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 
-export {};
+config({ path: '../../.env' });
+
+const client = postgres(process.env.DATABASE_URL!);
+export const db = drizzle({ client });
+
+export const profiles = pgTable('profiles', {
+    id: serial('id').primaryKey(), // foreign key to auth.users.id (don't think I can do this yet).
+    role: text('role').notNull(),
+    name: text('name').notNull(),
+    boolean: boolean('is_active'),
+});
+
+export const cohorts = pgTable('cohorts', {
+    id: serial('id').primaryKey(),
+    is_active: boolean('is_active'),
+});
+
+export const cohort_memberships = pgTable('cohort_membershps', {
+    cohort_id: serial('cohort_id').references(() => cohorts.id),
+    // still need to do user_id
+});
+
+export const modules = pgTable('modules', {
+    id: serial('id').primaryKey(),
+    module_index: integer('module_index').notNull().unique(),
+},
+    (table) => [
+        check("module_index_check", sql`0 < ${table.module_index} <= 6`),
+    ]
+);
