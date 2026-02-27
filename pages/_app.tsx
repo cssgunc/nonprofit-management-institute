4eeefdcb27f2bd50cb97ed7c10e7ba725b795b46
Maxwell Hu <maxhu@ad.unc.edu>
2026-02-26 22:45:42 -0500

    Built global header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { api } from "@/utils/trpc/api";
+import Header from "@/components/header";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Header />
+    <Component {...pageProps} />
+    </>
+  );
 }
 
 export default api.withTRPC(App);
