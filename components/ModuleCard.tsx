fb10ab246fe2415060689421da4d161531470a56
Yewon Song <yewons@unc.edu>
2026-02-28 23:05:37 -0500

    completed minimal styling for module card component

--- components/ModuleCard.tsx
+++ components/ModuleCard.tsx
@@ -12,9 +12,9 @@ export default function ModuleCard({
     className = "card",
 }: ModuleCardProps) {
     return (
-        <div className={` ${className}`}>
-            <h3>{title}</h3>
-            <p>{description}</p>
+        <div className={"w-[500px] h-[280px] bg-gray-300 flex flex-col items-center justify-center p-6 ${className}"}>
+            <h3 className="text-center text-black">{title}</h3>
+            <p className="text-center text-black">{description}</p>
         </div>
     );
 }
