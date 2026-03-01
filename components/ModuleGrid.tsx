0b6a1d7965531ff1b75a5141f71df212ed05bee0
Yewon Song <yewons@unc.edu>
2026-02-28 23:35:52 -0500

    completed routing the cards to specific module pages

--- components/ModuleGrid.tsx
+++ components/ModuleGrid.tsx
@@ -4,10 +4,12 @@ import { mockModules } from "@/server/mock/data";
 
 type ModuleGridProps = {
 	className?: string;
+	cohortSlug?: string;
 };
 
 export default function ModuleGrid({
 	className = "grid",
+	cohortSlug = "",
 }: ModuleGridProps) {
 	return (
         <div className={`grid grid-cols-2 md:grid-cols-3 gap-14 m-14 ${className}`}>
@@ -16,6 +18,8 @@ export default function ModuleGrid({
                     key={module.id}
                     title={module.title}
                     description={module.description || "No description available."}
+                    slug={module.slug}
+                    cohortSlug={cohortSlug}
                     className="card"
                 />
             ))}
--- pages/cohorts/[cohort_slug]/dashboard.tsx
+++ pages/cohorts/[cohort_slug]/dashboard.tsx
@@ -1,11 +1,15 @@
+import { useRouter } from "next/router";
 import ModuleGrid from "@/components/ModuleGrid";
 
 export default function DashboardPage() {
+	const router = useRouter();
+	const { cohort_slug } = router.query;
+
 	return (
         <div className='h-full w-full bg-blue'>
             {/* Header placeholder. Import /components/Header module here. */}
             <div className='h-25 w-full bg-white' />
-            <ModuleGrid />
+            <ModuleGrid cohortSlug={cohort_slug as string} />
         </div>
     ) 
 }
