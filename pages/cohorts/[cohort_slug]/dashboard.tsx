fb7f7fa8742a553a48b4dd75712f0233c28f1059
Yewon Song <yewons@unc.edu>
2026-02-28 22:57:23 -0500

    created the dashboard and added a header placeholder

--- pages/cohorts/[cohort_slug]/dashboard.tsx
+++ pages/cohorts/[cohort_slug]/dashboard.tsx
@@ -1,1 +1,11 @@
-// placeholder
+import ModuleGrid from "@/components/ModuleGrid";
+
+export default function DashboardPage() {
+	return (
+        <div className='h-full w-full bg-blue'>
+            {/* Header placeholder. Import /components/Header module here. */}
+            <div className='h-25 w-full bg-white' />
+            <ModuleGrid />
+        </div>
+    ) 
+}
