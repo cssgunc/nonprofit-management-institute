2e63c839d2e1f4660a183879193758b9866ae0cb
Pranshu Suyal <suyalpranshu@gmail.com>
2026-03-02 22:48:03 -0500

    Switch profiles id to uuid type

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -6,11 +6,11 @@
  * npx drizzle-kit push
  * ```
  */
-import { pgTable, text, boolean } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, boolean } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
 export const profiles = pgTable("profiles", {
-  id: text("id").primaryKey(),
+  id: uuid("id").primaryKey(),
   role: text("role", { enum: ["admin", "student"] }).notNull(),
   full_name: text("full_name").notNull(),
   is_active: boolean("is_active").notNull(),
