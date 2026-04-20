10f2da8649c266430f22b295c69127fc5586720d
dhruvnpUNC <dhruvnp@ad.unc.edu>
2026-04-20 08:17:38 -0400

    Created basic modal for discussion posts. CSS still needs to be implemented as well as a helper code to automatically insert module_id, cohort_id, and parent_post_id

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,10 @@
+import { useState } from 'react';
+import Modal from '../components/Modal';
+import { trpc } from '@/utils/trpc';
 export default function Home() {
+  const [open, setOpen] = useState<boolean>(false);
+  const [value, setValue] = useState('');
+  const createPost = trpc.discussions.createPost.useMutation();
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-black px-6">
       <h1 className="text-3xl font-bold text-white">Home</h1>
@@ -8,6 +14,45 @@ export default function Home() {
         /signup or /login and any unknown routes will be redirected to the 404
         page.
       </p>
+      <div>
+        <button
+          className="w-[228px] h-[44px] rounded-full py-[10px] px-[50px] gap-[10px] opacity-100"
+          onClick={() => setOpen(true)}
+        >
+          Make a Post
+      </button>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div>
+          <p>
+            Discussion
+          </p>
+          <input
+            type="text"
+            value = {value}
+            onChange = {(e) => setValue(e.target.value)}
+          >
+            Type your discussion here
+          </input>
+          <button
+            onClick={() => createPost.mutate({
+              value,
+              module_id: 1,
+              cohort_id: 1,
+              parent_post_id: null,
+            })}
+          >
+            Post
+          </button>
+          <button
+            className="w-[228px] h-[44px] rounded-full py-[10px] px-[50px] gap-[10px] opacity-100"
+            onClick={() => setOpen(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      </Modal>
+      
+      </div>
     </div>
   );
 }
