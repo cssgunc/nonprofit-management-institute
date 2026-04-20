c70abc806853fd34105e1fabaf30e55248308dd4
anikaahmed114 <aahm@unc.edu>
2026-04-20 14:37:17 -0400

    Lintingggg

--- components/ProfileMenu.tsx
+++ components/ProfileMenu.tsx
@@ -25,9 +25,7 @@ export default function ProfileMenu({
           className={`${className} rounded-full focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-500 focus-visible:ring-offset-2`}
         >
           <Avatar className="h-14 w-14 border border-gray-300 bg-gray-200 transition-brightness hover:brightness-90">
-            {avatarUrl && (
-              <AvatarImage src={avatarUrl} />
-            )}
+            {avatarUrl && <AvatarImage src={avatarUrl} />}
             <AvatarFallback className="text-sm font-semibold text-gray-700">
               {initials}
             </AvatarFallback>
